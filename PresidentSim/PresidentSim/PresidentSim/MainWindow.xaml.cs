6949a8ec99ad0e4df9b83814917aaee3772294da
Robin Ruzza <robin.ruzza@roche.com>
2021-05-10 16:01:21 +0200

    Added StartScreen

--- PresidentSim/PresidentSim/PresidentSim/MainWindow.xaml.cs
+++ PresidentSim/PresidentSim/PresidentSim/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace PresidentSim
         {
             InitializeComponent();
         }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
     }
 }
